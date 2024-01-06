a6f564a86b243850e306bc85fad3f21a2a697417
Erik Ferguson <eaferg510@gmail.com>
2024-01-06 14:58:55 -0500

    reorganize and add paths

--- scripts/create-conjugation-flashcards.py
+++ scripts/create-conjugation-flashcards.py
@@ -3,6 +3,8 @@ import os
 import json
 import pandas as pd
 from sys import exit
+from pathlib import Path
+from dotenv import load_dotenv
 
 class Flashcard():
     def __init__(self):
@@ -30,17 +32,19 @@ class Flashcard():
 verb = input("Type in the verb we need to conjugate: ")
 
 #Begin check if verb exists
-df = pd.read_csv('conjugations-db.csv',header=0)
+df = pd.read_csv(Path('outputs/conjugations-db.csv'),header=0)
 if verb in df['Infinitive'].values:
     print('Verb has already been added to flashcard csv')
     exit()
+
+load_dotenv()
 
 #Initialize call to ChatGPT
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_SECRET"),
 )
 
-with open('conjugation-schema.json','r') as f:
+with open('templates/conjugation-schema.json','r') as f:
     json_format = f.read()
 
 question = f'Can you give me the conjugations of the Spanish verb "{verb}" in the form of a JSON object formatted as such where "conj" is replaced by the conjugated form of the verb for the correct mood, tense, and person:'
@@ -90,4 +94,4 @@ for key in ["Indicative", "Subjunctive","Imperative"]:
                 flashcard.ex_eng = ''
                 flashcard.export(df=df)
 
-df.to_csv('conjugations-db.csv')
+df.to_csv(Path('outputs/conjugations-db.csv'), index=False)
